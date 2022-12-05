34d58b46e52270b78176d532ff9fc5b3af5bb65b
Animesh <anivad03@gmail.com>
2022-12-05 19:54:40 +0530

    clean tokenizer

--- includes/tokenizer.h
+++ includes/tokenizer.h
@@ -6,22 +6,21 @@
 #include <string.h>
 #include <stdlib.h>
 
-// Returns 'true' if the character is a DELIMITER.
-bool isDelimiter(char ch)
-{
-	if (ch == ' ' || ch == '+' || ch == '-' || ch == '*' ||
-		ch == '/' || ch == ',' || ch == ';' || ch == '>' ||
-		ch == '<' || ch == '=' || ch == '(' || ch == ')' ||
-		ch == '[' || ch == ']' || ch == '{' || ch == '}' || ch == '\n')
-		return (true);
-	return (false);
-}
-
 // Returns 'true' if the character is an OPERATOR.
 bool isOperator(char ch)
 {
 	if (ch == '+' || ch == '-' || ch == '*' ||
 		ch == '/' || ch == '>' || ch == '=')
+		return (true);
+	return (false);
+}
+
+// Returns 'true' if the character is a DELIMITER.
+bool isDelimiter(char ch)
+{
+	if (ch == ' ' || ch == ',' || ch == ';' || ch == '>' ||
+		ch == '=' || ch == '(' || ch == ')' ||
+		ch == '{' || ch == '}' || ch == '\n' || isOperator(ch))
 		return (true);
 	return (false);
 }
@@ -53,10 +52,7 @@ bool isInteger(char* str)
 	if (len == 0)
 		return (false);
 	for (i = 0; i < len; i++) {
-		if (str[i] != '0' && str[i] != '1' && str[i] != '2'
-			&& str[i] != '3' && str[i] != '4' && str[i] != '5'
-			&& str[i] != '6' && str[i] != '7' && str[i] != '8'
-			&& str[i] != '9' || (str[i] == '-' && i > 0))
+		if ((str[i] < '0' || str[i] > '9') || (str[i] == '-' && i > 0))
 			return (false);
 	}
 	return (true);
